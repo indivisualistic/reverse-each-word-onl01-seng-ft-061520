ef4e65a318c406d39a2088bcbe2ef706a18c5720
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-19 15:57:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,8 @@
 def reverse_each_word(sentence1)
+  words = sentence.split(" ")
+  reversed_sentence = []
+  words.collect do |word|
+    eversed_sentence << word.reverse
+end
+reversed_sentence.join(" ")
 end
